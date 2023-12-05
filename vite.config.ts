23cf9d978c0bf657732755a9b21e62baf7d02121
ycl <499475142@qq.com>
2023-12-05 14:11:30 +0800

    修改配置

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,13 @@ export default defineConfig({
     viteMockServe({
       mockPath: './mock/', //设置模拟数据的存储文件夹
       logger: true, // 是否在控制台显示请求日志
-      localEnabled: false, //设置是否启用本地mock文件
+      localEnabled: true, //设置是否启用本地mock文件
       prodEnabled: true // 设置打包是否启用mock功能
     })
   ],
+  build: {
+    sourcemap: true
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, './src')
