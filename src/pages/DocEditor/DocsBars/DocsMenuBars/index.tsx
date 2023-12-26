5ec5e5716ee49b0caa79747d36fa7f33acccafa9
yeconglin <499475142@qq.com>
2023-12-26 09:07:15 +0800

    解决掉在一个组件中，使用两个dropdown时，定义两个下拉选项的问题，解决办法参考https://github.com/ant-design/ant-design/issues/41378

--- src/pages/DocEditor/DocsBars/DocsMenuBars/index.tsx
+++ src/pages/DocEditor/DocsBars/DocsMenuBars/index.tsx
@@ -1,10 +1,67 @@
 import React from "react";
+import {Dropdown} from "antd";
+import {EditOutlined, LogoutOutlined, SyncOutlined, UserOutlined} from "@ant-design/icons";
+import {useNavigate} from "react-router-dom";
 
 const DocsMenuBars: React.FC = () => {
+  const navigate = useNavigate();
+  const items = [
+    {
+      key: '1',
+      label: '个人中心',
+      icon: <UserOutlined/>,
+      disabled: false,
+      onClick: () => {
+        navigate('system/personal')
+      }
+    },
+    {
+      key: '2',
+      label: '密码修改',
+      icon: <EditOutlined/>,
+      onClick: () => {
+        alert('进行密码修改')
+      }
+    },
+    {
+      key: '3',
+      label: '刷新缓存',
+      icon: <SyncOutlined/>,
+      onClick: () => {
+        alert('刷新缓存操作')
+      }
+    },
+
+  ]
+  const items2 = [
+    {
+      key: '4',
+      label: '退出登录',
+      icon: <LogoutOutlined/>,
+      disabled: false,
+      onClick: function () {
+
+      }
+    }
+  ]
+
+  const menuProp = {
+    items
+  }
+
+  const menuProps2 = {
+    items: items2
+  }
+
   return (
     <>
       <div id="docs-menubars">
-        菜单栏
+        <Dropdown menu={menuProp}>
+          <span>文件</span>
+        </Dropdown>
+        <Dropdown menu={menuProps2}>
+          <span>编辑</span>
+        </Dropdown>
       </div>
     </>
   )
