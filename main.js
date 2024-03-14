e6d09646e15ab3ba282ee34b62a8293e6d9851cb
yeconglin <499475142@qq.com>
2024-03-14 21:10:25 +0800

    electron

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function createWindow() {
     mainWindow.loadURL('http://localhost:3000')
     // mainWindow.webContents.openDevTools()
   } else {
-    mainWindow.loadFile(path.join(__dirname, '../integration', 'index.html'))
+    mainWindow.loadFile(path.join(__dirname, './integration', 'index.html'))
   }
   mainWindow.show();
 }
