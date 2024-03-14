d4f4d0521532dde3bccd655038f75f750d0a945b
ycl <499475142@qq.com>
2024-03-14 11:18:20 +0800

    添加前端console控制台用于sql执行监控或者是错误监控

---
+++ src/components/console/console.tsx
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from "react";
+import {Modal} from "antd";
+
+/**
+ * 前端使用的SQL、错误监控台
+ */
+const Console: React.FC = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  // 初始化的时候绑定键盘事件
+  useEffect(() => {
+    // 监听键盘事件
+    document.addEventListener("keyup", keyupEvent, false);
+  });
+
+  const keyupEvent = (e: KeyboardEvent) => {
+    if (e.code === "Escape") {
+      setOpen(false);
+    }
+    if (e.ctrlKey && e.keyCode === 123) {
+      setOpen(true);
+    }
+  }
+
+  return (
+    <>
+      <Modal
+        open={open}
+        mask={false}
+        title={<span>console监控台</span>}
+        style={{top: 20, right: 20, position: "absolute"}}
+        styles={{
+          body: {
+            height: "calc(100vh - 160px)",
+            border: "1px solid #ccc",
+            padding: "10px",
+            borderRadius: "6px",
+            backgroundColor: "aliceblue"
+          }
+        }}
+        footer={null}
+        onCancel={() => setOpen(false)}
+      >
+        console监控台
+      </Modal>
+    </>
+  )
+}
+export default Console;
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,16 +3,21 @@ import LeftMenu from "@/layouts/LeftMenu";
 import {Layout} from "antd";
 import Header from "@/layouts/Header";
 import Content from "@/layouts/Content";
+import Console from "@/components/console/console.tsx";
 /* 页面布局组件 */
 const Layouts: React.FC = () => {
   return (
-    <Layout style={{height: "100%"}}>
-      <Header/>
-      <Layout style={{backgroundColor: '#fff'}}>
-        <LeftMenu/>
-        <Content/>
+    <>
+      {/* 监控台 */}
+      <Console/>
+      <Layout style={{height: "100%"}}>
+        <Header/>
+        <Layout style={{backgroundColor: '#fff'}}>
+          <LeftMenu/>
+          <Content/>
+        </Layout>
       </Layout>
-    </Layout>
+    </>
   )
 }
 export default Layouts;
