5aeefed2625be923eab37963f79051a27488d6d7
ycl <499475142@qq.com>
2024-05-09 09:48:12 +0800

    修改菜单请求路径

--- src/services/system/permission/menuApi.ts
+++ src/services/system/permission/menuApi.ts
@@ -1,14 +1,14 @@
 export enum Api {
   // 根据token获取菜单（多用于框架上根据角色获取菜单那种）
-  getMenuList = '/api/menu/getMenusByRole',
+  getMenuList = '/api/system/menu/getMenusByRole',
   // 获取所有菜单
-  getAllMenus = '/api/menu/getAllMenus',
+  getAllMenus = '/api/system/menu/getAllMenus',
   // 获取所有上级菜单
-  getDirectoryMenu = '/api/menu/getDirectoryMenu',
+  getDirectoryMenu = '/api/system/menu/getDirectoryMenu',
   // 添加菜单
-  addPermission = '/api/menu/addPermission',
+  addPermission = '/api/system/menu/addPermission',
   // 编辑菜单
-  updatePermission = '/api/menu/updatePermission',
+  updatePermission = '/api/system/menu/updatePermission',
   // 删除菜单
-  deletePermission = '/api/menu/deletePermission'
+  deletePermission = '/api/system/menu/deletePermission'
 }
