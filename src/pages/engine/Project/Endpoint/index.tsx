b3ba793d676e9ac0ab1e0a1e50cacd9d4df4ec1b
ycl <499475142@qq.com>
2024-06-03 17:54:14 +0800

    端点管理界面

--- src/pages/engine/Project/Endpoint/index.tsx
+++ src/pages/engine/Project/Endpoint/index.tsx
@@ -1,4 +1,12 @@
-import { DeleteOutlined, ExportOutlined, ImportOutlined, PlusOutlined, SearchOutlined, SyncOutlined } from "@ant-design/icons";
+import {
+  DeleteOutlined,
+  ExportOutlined,
+  HomeOutlined,
+  ImportOutlined,
+  PlusOutlined,
+  SearchOutlined,
+  SyncOutlined
+} from "@ant-design/icons";
 import {
   Button,
   Card,
@@ -9,12 +17,13 @@ import {
   Row,
   Select,
   Space,
-  Table,
+  Table, Tooltip,
 } from "antd";
-import React, { useEffect, useRef, useState } from "react";
-import { Endpoint } from "./Endpoint";
-import { ColumnsType, TableRowSelection } from "antd/es/table/interface";
-const Index: React.FC = ()=> {
+import React, {useEffect, useRef, useState} from "react";
+import {Endpoint} from "./Endpoint";
+import {ColumnsType, TableRowSelection} from "antd/es/table/interface";
+
+const Index: React.FC = () => {
   const [searchForm] = Form.useForm();
   const endpointName = useRef<InputRef>(null);
 
@@ -102,67 +111,83 @@ const Index: React.FC = ()=> {
   ]
 
   return (
-    <>
-      {/* 查询区域 */}
-      <Card styles={{body: {height: '100%'}}} style={{marginBottom: '16px'}}>
-        <Form form={searchForm} onFinish={finishSearch} initialValues={{projectType: '-1'}}>
-          <Row gutter={24}>
-            <Col span={6}>
-              <Form.Item label="端点名称" name="name" style={{marginBottom: 0}}>
-                <Input ref={endpointName} autoFocus allowClear autoComplete="false"/>
-              </Form.Item>
-            </Col>
-            <Col span={6}>
-              <Form.Item label="端点类型" name="type" style={{marginBottom: 0}}>
-                <Input allowClear autoComplete="false"/>
-              </Form.Item>
-            </Col>
-            <Col span={6}>
-              <Form.Item label="模式" name="mode" style={{marginBottom: 0}}>
-                <Select defaultValue="-1" options={[
-                  {value: '-1', label: '请选择模式', disabled: true},
-                  {value: '1', label: 'IN'},
-                  {value: '2', label: 'IN_OUT'},
-                  {value: '3', label: 'OUT'},
-                  {value: '4', label: 'OUT_IN'}
-                ]}/>
-              </Form.Item>
-            </Col>
-            
-            <Col span={6}>
-              <Button type="primary" htmlType="submit"><SearchOutlined/>查询</Button>
-              <Button htmlType="reset" style={{margin: '0 8px'}}><SyncOutlined/>重置</Button>
-            </Col>
-          </Row>
-        </Form>
-      </Card>
-      <Card>
-        <section style={{marginBottom: '16px'}}>
-          <Space wrap>
-            <Button type="primary" onClick={() => {}} icon={<PlusOutlined/>}>新建</Button>
-            <Button type="primary" onClick={()=>{alert('批量操作')}} icon={<DeleteOutlined style={{color: selectRow.length === 0 ? '#ccc' : 'red'}}/>} disabled={selectRow.length === 0}>批量操作</Button>
-            <Button type="default" onClick={() => alert('导入')} icon={<ImportOutlined style={{color: 'orange'}}/>}>导入</Button>
-            <Button type="default" onClick={() => alert('导出')} icon={<ExportOutlined style={{color: selectRow.length === 0 ? '#ccc' : 'red'}}/>} disabled={selectRow.length === 0}>导出</Button>
-          </Space>
-        </section>
-        <section>
-          <Table
-            rowSelection={{...rowSelection, checkStrictly: false}}
-            scroll={{x: '100', y: 'calc(100vh - 270px)'}}
-            style={{marginTop: '6px'}}
-            size="middle"
-            pagination={{
-              showQuickJumper: true,
-              showSizeChanger: true,
-              defaultPageSize: 25,
-              total: 1,
-              showTotal: (total) => `共 ${total} 条`
-            }}
-            columns={columns}
-            dataSource={endpointSource}
-          /></section>
-      </Card>
-    </>
+      <>
+        {/* 查询区域 */}
+        <Card styles={{body: {height: '100%'}}} style={{marginBottom: '16px'}}>
+          <Form form={searchForm} onFinish={finishSearch} initialValues={{projectType: '-1', mode: '-1'}}>
+            <Row gutter={24}>
+              <Col span={6}>
+                <Form.Item label="端点名称" name="name" style={{marginBottom: 0}}>
+                  <Input ref={endpointName} autoFocus allowClear autoComplete="false"/>
+                </Form.Item>
+              </Col>
+              <Col span={6}>
+                <Form.Item label="端点类型" name="type" style={{marginBottom: 0}}>
+                  <Input allowClear autoComplete="false"/>
+                </Form.Item>
+              </Col>
+              <Col span={6}>
+                <Form.Item label="模式" name="mode" style={{marginBottom: 0}}>
+                  <Select options={[
+                    {value: '-1', label: '请选择模式', disabled: true},
+                    {value: '1', label: 'IN'},
+                    {value: '2', label: 'IN_OUT'},
+                    {value: '3', label: 'OUT'},
+                    {value: '4', label: 'OUT_IN'}
+                  ]}/>
+                </Form.Item>
+              </Col>
+
+              <Col span={6}>
+                <Button type="primary" htmlType="submit"><SearchOutlined/>查询</Button>
+                <Button htmlType="reset" style={{margin: '0 8px'}}><SyncOutlined/>重置</Button>
+              </Col>
+            </Row>
+          </Form>
+        </Card>
+        <Card>
+          <section style={{marginBottom: '16px'}}>
+            <Row>
+              <Col span={22}>
+                <Space wrap>
+                  <Button type="primary" onClick={() => {
+                  }} icon={<PlusOutlined/>}>新建</Button>
+                  <Button type="primary" onClick={() => {
+                    alert('批量操作')
+                  }} icon={<DeleteOutlined style={{color: selectRow.length === 0 ? '#ccc' : 'red'}}/>}
+                          disabled={selectRow.length === 0}>批量操作</Button>
+                  <Button type="default" onClick={() => alert('导入')}
+                          icon={<ImportOutlined style={{color: 'orange'}}/>}>导入</Button>
+                  <Button type="default" onClick={() => alert('导出')}
+                          icon={<ExportOutlined style={{color: selectRow.length === 0 ? '#ccc' : 'red'}}/>}
+                          disabled={selectRow.length === 0}>导出</Button>
+                </Space>
+              </Col>
+              <Col span={2} style={{textAlign: 'right'}}>
+                <Tooltip title="回收站" color="blue" key="blue">
+                  <Button type="primary" shape="circle" icon={<HomeOutlined/>} onClick={() => {alert('打开回收站')}}/>
+                </Tooltip>
+              </Col>
+            </Row>
+          </section>
+          <section>
+            <Table
+                rowSelection={{...rowSelection, checkStrictly: false}}
+                scroll={{x: '100', y: 'calc(100vh - 270px)'}}
+                style={{marginTop: '6px'}}
+                size="middle"
+                pagination={{
+                  showQuickJumper: true,
+                  showSizeChanger: true,
+                  defaultPageSize: 25,
+                  total: 1,
+                  showTotal: (total) => `共 ${total} 条`
+                }}
+                columns={columns}
+                dataSource={endpointSource}
+            /></section>
+        </Card>
+      </>
   );
 }
 export default Index;
