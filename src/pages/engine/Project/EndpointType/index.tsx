341d8c9247cc62b87db300b71e238fdc1a424a5a
ycl <499475142@qq.com>
2024-06-18 10:47:00 +0800

    调整端点类型左边表格，使其占满一页高度

--- src/pages/engine/Project/EndpointType/index.tsx
+++ src/pages/engine/Project/EndpointType/index.tsx
@@ -1,9 +1,12 @@
-import { Card, Col, Row, Input, Table } from "antd";
+import {Card, Col, Row, Input, Table} from "antd";
 import React from "react";
-import { ColumnsType } from "antd/es/table";
-import { EndpointTypeModel } from "./endpointType";
+import {ColumnsType} from "antd/es/table";
+import {EndpointTypeModel} from "./endpointType";
 
-const { Search } = Input;
+const {Search} = Input;
+import "./index.less";
+
+
 const EndpointType: React.FC = () => {
   /**
    * 类型表格列
@@ -30,48 +33,50 @@ const EndpointType: React.FC = () => {
     console.log(params);
   };
   return (
-    <Row gutter={8} style={{ height: "100%" }}>
-      {/* 左边表格布局 */}
-      <Col span={6}>
-        <Card style={{ height: "100%" }} styles={{ body: { height: "100%" } }}>
-          <section style={{ marginBottom: "16px" }}>
-            <Search
-              autoFocus
-              placeholder="请输入端点类型名或支持模式进行检索"
-              onSearch={onSearch}
-              enterButton
-            />
-          </section>
-          <section style={{height: "calc(100% - 48px)", display: "flex", flexDirection: "column"}}>
-            <Table
-              scroll={{ x: "100", y: "calc(100vh - 160px)" }}
-              style={{ marginTop: "6px", height: '100%' }}
-              size="middle"
-              pagination={{
-                showQuickJumper: true,
-                showSizeChanger: true,
-                defaultPageSize: 25,
-                total: 1,
-                showTotal: (total) => `共 ${total} 条`,
-              }}
-              columns={typeTableColumn}
-            />
-          </section>
-        </Card>
-      </Col>
-      {/* 右边表单布局 */}
-      <Col span={18}>
-        <Card style={{ height: "150px" }} styles={{ body: { height: "100%" } }}>
-          基础配置，名称-类型
-        </Card>
-        <Card
-          style={{ height: "calc(100% - 156px)", marginTop: "6px" }}
-          styles={{ body: { height: "100%" } }}
-        >
-          编辑表格部分（配置项）
-        </Card>
-      </Col>
-    </Row>
+      <Row gutter={8} style={{height: "100%"}}>
+        {/* 左边表格布局 */}
+        <Col span={6}>
+          <Card style={{height: "100%"}} styles={{body: {height: "100%", padding: '24px 24px 0 24px'}}}>
+            <section style={{marginBottom: "16px"}}>
+              <Search
+                  autoFocus
+                  placeholder="请输入端点类型名或支持模式进行检索"
+                  onSearch={onSearch}
+                  enterButton
+              />
+            </section>
+            <section style={{height: "calc(100% - 48px)", display: "flex", flexDirection: "column"}}>
+              <Table
+                  bordered
+                  className="endpointTypeTable"
+                  scroll={{x: "100", y: "calc(100vh - 240px)"}}
+                  style={{marginTop: "6px", height: '100%'}}
+                  size="middle"
+                  pagination={{
+                    showQuickJumper: true,
+                    showSizeChanger: true,
+                    defaultPageSize: 25,
+                    total: 1,
+                    showTotal: (total) => `共 ${total} 条`,
+                  }}
+                  columns={typeTableColumn}
+              />
+            </section>
+          </Card>
+        </Col>
+        {/* 右边表单布局 */}
+        <Col span={18}>
+          <Card style={{height: "150px"}} styles={{body: {height: "100%"}}}>
+            基础配置，名称-类型
+          </Card>
+          <Card
+              style={{height: "calc(100% - 156px)", marginTop: "6px"}}
+              styles={{body: {height: "100%"}}}
+          >
+            编辑表格部分（配置项）
+          </Card>
+        </Col>
+      </Row>
   );
 };
 export default EndpointType;
