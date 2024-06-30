2497c8fa5f6dcba997a76f9e1ace0bce1ccfb9a5
yeconglin <499475142@qq.com>
2024-06-30 17:25:58 +0800

    区分在开发模式下开启严格模式可以进行错误的预防性检测

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+if (import.meta.env.MODE === 'development') {
+  root.render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  )
+} else {
+  root.render(
     <BrowserRouter>
-      <App/>
+      <App />
     </BrowserRouter>
-)
+  )
+}
