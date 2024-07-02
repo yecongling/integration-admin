a3d473727a7390f686fbfe87b56c77e1df06d229
ycl <499475142@qq.com>
2024-07-02 17:33:37 +0800

    1、升级vite 2、配置打包做gzip压缩

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,41 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
-import {viteMockServe} from "vite-plugin-mock";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { viteMockServe } from "vite-plugin-mock";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    viteCompression({
+      verbose: false,
+      deleteOriginFile: true,
+    }),
     viteMockServe({
-      mockPath: './mock/', //设置模拟数据的存储文件夹
+      mockPath: "./mock/", //设置模拟数据的存储文件夹
       logger: true, // 是否在控制台显示请求日志
       localEnabled: true, //设置是否启用本地mock文件
-      prodEnabled: false // 设置打包是否启用mock功能
-    })
+      prodEnabled: false, // 设置打包是否启用mock功能
+    }),
   ],
   build: {
     sourcemap: true,
-    outDir: 'integration',
+    outDir: "integration",
     rollupOptions: {
       output: {
         manualChunks: {
-          react: ['react', 'react-dom', 'react-router-dom', 'zustand'],
-          antd: ['antd'],
+          react: ["react", "react-dom", "react-router-dom", "zustand"],
+          antd: ["antd"],
         },
       },
     },
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, './src')
+      "@": path.resolve(__dirname, "./src"),
     },
-    extensions: [".js", ".ts", ".jsx", ".tsx", ".json"]
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".json"],
   },
   server: {
     port: 3000,
@@ -38,8 +43,8 @@ export default defineConfig({
       "/api": {
         target: "http://localhost:8080/integration",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
-    }
-  }
-})
+    },
+  },
+});
