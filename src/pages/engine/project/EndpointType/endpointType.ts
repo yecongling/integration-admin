88071f30866d07cb9e928c4b434a2a71988af364
yeconglin <499475142@qq.com>
2024-07-09 21:38:37 +0800

    1、引入pro-components，后续考虑使用antd的table自己封装一个编辑表格 2、端点类型管理界面添加编辑表格

--- src/pages/engine/project/EndpointType/endpointType.ts
+++ src/pages/engine/project/EndpointType/endpointType.ts
@@ -1,3 +1,5 @@
+import React from "react";
+
 export interface EndpointTypeModel {
   /** 端点类型ID */
   id: string;
@@ -7,4 +9,17 @@ export interface EndpointTypeModel {
   description: string;
   /** 端点类型支持的模式 */
   supportModeList: string[];
+  // 支持的模式
+  supportMode: string;
+}
+
+export type DataSourceType = {
+  id: React.Key;
+  name?: string;
+  type?: string;
+  readonly?: string;
+  desc?: string;
+  state?: string;
+  create_time?: string;
+  update_time?: string;
 }
--- src/pages/engine/project/EndpointType/index.tsx
+++ src/pages/engine/project/EndpointType/index.tsx
@@ -1,9 +1,11 @@
-import {Card, Col, Row, Input, Table} from "antd";
-import React from "react";
-import {ColumnsType} from "antd/es/table";
-import {EndpointTypeModel} from "./endpointType";
+import { Card, Col, Row, Input, Table } from "antd";
+import React, { useState } from "react";
+import { ColumnsType } from "antd/es/table";
+import { DataSourceType, EndpointTypeModel } from "./endpointType";
+import type { ProColumns } from "@ant-design/pro-components";
+import { EditableProTable } from "@ant-design/pro-components";
 
-const {Search} = Input;
+const { Search } = Input;
 import "./index.less";
 
 
@@ -20,10 +22,110 @@ const EndpointType: React.FC = () => {
     {
       title: "名称",
       dataIndex: "name",
-      width: "20%",
+      width: "40%",
       align: "left",
     },
+    {
+      title: "支持模式",
+      dataIndex: "supportMode",
+      width: "60%",
+      align: "left"
+    }
   ];
+
+  // 编辑列
+  const [editableKeys, setEditableRowKeys] = useState<React.Key[]>([]);
+  // 属性表格数据
+  const [properties, setProperties] = useState<readonly DataSourceType[]>([
+    {
+      id: '624748504',
+      name: '活动名称一',
+      type: "open",
+      state: 'open',
+      create_time: "",
+      update_time: "",
+    },
+    {
+      id: '624691229',
+      name: '活动名称二',
+      state: 'closed',
+      create_time: "1590481162000",
+      update_time: "1590481162000",
+    },
+  ]);
+
+  // 属性表格列
+  const propertiesColumns: ProColumns<DataSourceType>[] = [
+    {
+      title: "属性名称",
+      dataIndex: "name",
+      formItemProps: () => {
+        return {
+          rules: [{ required: true, message: "属性名称必填" }]
+        }
+      },
+    },
+    {
+      title: "属性类型",
+      dataIndex: "type",
+      valueType: "select",
+      valueEnum: {
+        all: { text: '全部', status: 'Default' },
+        open: {
+          text: '未解决',
+          status: 'Error',
+        },
+        closed: {
+          text: '已解决',
+          status: 'Success',
+        },
+      },
+    },
+    {
+      title: '状态',
+      key: 'state',
+      dataIndex: 'state',
+      valueType: 'select',
+      valueEnum: {
+        all: { text: '全部', status: 'Default' },
+        open: {
+          text: '未解决',
+          status: 'Error',
+        },
+        closed: {
+          text: '已解决',
+          status: 'Success',
+        },
+      },
+    },
+
+    {
+      title: '活动时间',
+      dataIndex: 'created_at',
+      valueType: 'date',
+    },
+    {
+      title: '操作',
+      valueType: 'option',
+      width: 200,
+      render: (_text, record, _, action) => [
+        <a
+          key="editable"
+          onClick={() => {
+            action?.startEditable?.(record.id);
+          }}
+        >
+          编辑
+        </a>,
+        <a
+          key="delete"
+          onClick={() => { }}
+        >
+          删除
+        </a>,
+      ],
+    },
+  ]
 
   /**
    * 检索功能
@@ -33,50 +135,73 @@ const EndpointType: React.FC = () => {
     console.log(params);
   };
   return (
-      <Row gutter={8} style={{height: "100%"}}>
-        {/* 左边表格布局 */}
-        <Col span={6}>
-          <Card style={{height: "100%"}} styles={{body: {height: "100%", padding: '24px 24px 0 24px'}}}>
-            <section style={{marginBottom: "16px"}}>
-              <Search
-                  autoFocus
-                  placeholder="请输入端点类型名或支持模式进行检索"
-                  onSearch={onSearch}
-                  enterButton
-              />
-            </section>
-            <section style={{height: "calc(100% - 48px)", display: "flex", flexDirection: "column"}}>
-              <Table
-                  bordered
-                  className="endpointTypeTable"
-                  scroll={{x: "100", y: "calc(100vh - 240px)"}}
-                  style={{marginTop: "6px", height: '100%'}}
-                  size="middle"
-                  pagination={{
-                    showQuickJumper: true,
-                    showSizeChanger: true,
-                    defaultPageSize: 25,
-                    total: 1,
-                    showTotal: (total) => `共 ${total} 条`,
-                  }}
-                  columns={typeTableColumn}
-              />
-            </section>
-          </Card>
-        </Col>
-        {/* 右边表单布局 */}
-        <Col span={18}>
-          <Card style={{height: "150px"}} styles={{body: {height: "100%"}}}>
-            基础配置，名称-类型
-          </Card>
-          <Card
-              style={{height: "calc(100% - 156px)", marginTop: "6px"}}
-              styles={{body: {height: "100%"}}}
-          >
-            编辑表格部分（配置项）
-          </Card>
-        </Col>
-      </Row>
+    <Row gutter={8} style={{ height: "100%" }}>
+      {/* 左边表格布局 */}
+      <Col span={6}>
+        <Card style={{ height: "100%" }} styles={{ body: { height: "100%", padding: '24px 24px 0 24px' } }}>
+          <section style={{ marginBottom: "16px" }}>
+            <Search
+              autoFocus
+              placeholder="请输入端点类型名或支持模式进行检索"
+              onSearch={onSearch}
+              enterButton
+            />
+          </section>
+          <section style={{ height: "calc(100% - 48px)", display: "flex", flexDirection: "column" }}>
+            <Table
+              bordered
+              className="endpointTypeTable"
+              scroll={{ x: "100", y: "calc(100vh - 240px)" }}
+              style={{ marginTop: "6px", height: '100%' }}
+              size="middle"
+              pagination={{
+                showQuickJumper: true,
+                showSizeChanger: true,
+                defaultPageSize: 25,
+                total: 1,
+                showTotal: (total) => `共 ${total} 条`,
+              }}
+              columns={typeTableColumn}
+            />
+          </section>
+        </Card>
+      </Col>
+      {/* 右边表单布局 */}
+      <Col span={18}>
+        <Card style={{ height: "250px" }} styles={{ body: { height: "100%" } }}>
+          基础配置，名称-类型
+        </Card>
+        <Card
+          style={{ height: "calc(100% - 256px)", marginTop: "6px" }}
+          styles={{ body: { height: "100%" } }}
+        >
+          <EditableProTable<DataSourceType>
+            rowKey="id"
+            headerTitle="端点类型属性"
+            maxLength={5}
+            scroll={{ x: 960 }}
+            loading={false}
+            value={properties}
+            columns={propertiesColumns}
+            onChange={setProperties}
+            editable={{
+              type: "multiple",
+              editableKeys,
+              onSave: async (rowKey, data, row) => {
+                console.log(rowKey, data, row);
+              },
+              onChange: setEditableRowKeys
+            }}
+            recordCreatorProps={
+              {
+                position: "bottom",
+                record: () => ({ id: (Math.random() * 1000000).toFixed(0) }),
+              }
+            }
+          />
+        </Card>
+      </Col>
+    </Row>
   );
 };
 export default EndpointType;
